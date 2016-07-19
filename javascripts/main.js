4488de5dfd904a6043eef820bd2ac940d5d367e2
joshua <d3c1m80r@gmail.com>
2016-07-19 00:16:11 -0500

    made the code readable, added a dropdown box for projects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('This would be the main JS file.');
+
+//code from http://www.w3schools.com/howto/howto_js_dropdown.asp
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+/*function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}*/
+//end copied code
