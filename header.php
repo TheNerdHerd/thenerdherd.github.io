7658b27cc636ecb2246e2012f5abdc7bf016e017
Mike Stache <whitefolkgotnothingonme@gmail.com>
2017-03-11 22:47:30 -0500

    update 3-11-17 part 2
    deleted header.php, fixed links

--- header.php
+++
@@ -1,8 +0,0 @@
-<!-- this isn't working yet... trying to install php on my computer -->
-<body>
-	<div id = "nav">
-		<h1>
-			<img href = 'images/protocal_banner.png'>
-		</h1>
-		<a href = "protocal.php">Home</a>
-	</div>
